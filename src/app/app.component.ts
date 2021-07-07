30f9ed02e93b724c09e8c683845bf064e44271c6
Matthew Berry <mjberry@illinois.edu>
2021-07-07 10:32:19 -0700

    initialized project with ng new

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dmm-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'digital-molecule-maker';
+}
