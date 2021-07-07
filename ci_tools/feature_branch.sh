856df9b6bec53425ca7bbcc421dd99cc418f8819
Matthew Berry <mjberry@illinois.edu>
2021-07-07 10:55:17 -0700

    replaced references to pixsure

--- ci_tools/feature_branch.sh
+++ ci_tools/feature_branch.sh
@@ -4,12 +4,12 @@ CI_SCRIPT_DIR=`dirname $0`
 
 # stop any containers still running from any previous runs
 # filter out common error messages that container isn't found
-docker stop pixsure_dev 2>&1 | grep -v "no such id" | grep -v "failed to stop"
-docker rm pixsure_dev 2>&1 | grep -v "no such id" |grep -v "failed to remove"
+docker stop dmm_dev 2>&1 | grep -v "no such id" | grep -v "failed to stop"
+docker rm dmm_dev 2>&1 | grep -v "no such id" |grep -v "failed to remove"
 
 cd $CI_SCRIPT_DIR && \
-    sh build_pixsure_dev.sh && \
-    INTERACTIVE_FLAGS='' sh run_pixsure_dev.sh npm install && \
-    INTERACTIVE_FLAGS='' sh run_pixsure_dev.sh ng build && \
-    INTERACTIVE_FLAGS='' sh run_pixsure_dev.sh ng test && \
-    INTERACTIVE_FLAGS='' sh run_pixsure_dev.sh ng e2e
+    sh build_dmm_dev.sh && \
+    INTERACTIVE_FLAGS='' sh run_dmm_dev.sh npm install && \
+    INTERACTIVE_FLAGS='' sh run_dmm_dev.sh ng build && \
+    INTERACTIVE_FLAGS='' sh run_dmm_dev.sh ng test && \
+    INTERACTIVE_FLAGS='' sh run_dmm_dev.sh ng e2e
