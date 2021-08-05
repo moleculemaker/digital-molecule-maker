333926c110fc034ff4999a1c1a6fdb4c248ac871
Matthew Berry <mjberry@illinois.edu>
2021-08-05 10:43:05 -0500

    made mask ids unique

--- src/app/block/block.component.ts
+++ src/app/block/block.component.ts
@@ -30,10 +30,20 @@ export class BlockComponent implements OnInit {
     height: 68
   };
 
+  id: string;
+
   borderRadius:Number = 8;
 
-  constructor() { }
+  constructor() {
+    this.id = this.uuidv4();
+   }
 
+  uuidv4(): string {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+  }
   //********************************************
   ngOnInit(): void {
     //change border size for icons
