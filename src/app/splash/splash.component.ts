26ef75422444f9ffe85f7903b7a900877665fd8f
Chad Olson <chad@function49.com>
2022-04-29 16:35:07 -0500

    first pass on splash layout and animations

---
+++ src/app/splash/splash.component.ts
@@ -0,0 +1,57 @@
+import { Component, HostListener, Input, OnInit, ElementRef, ViewChild } from '@angular/core';
+
+//import "external-svg-loader";
+
+@Component({
+  selector: 'splash',
+  templateUrl: './splash.component.html',
+  styleUrls: ['./splash.component.scss']
+})
+export class SplashComponent implements OnInit {
+  @ViewChild('blueLeftEye') blueLeftEye: ElementRef|null = null;
+  @ViewChild('blueRightEye') blueRightEye: ElementRef|null = null;
+
+  @HostListener('document:mousemove', ['$event']) onMouseMove(e:MouseEvent) {this.mouseMove(e);}
+
+  constructor() { }
+
+  //********************************************
+  ngOnInit(): void {
+  }
+
+  //********************************************
+  ngAfterViewInit() {
+    //setup eye movements
+  }
+
+  //********************************************
+  mouseMove(e:MouseEvent) {
+    const eyeOffset = -135;
+
+    if (this.blueLeftEye) {
+      var rect = this.blueLeftEye.nativeElement.getBoundingClientRect();
+
+      var mouse = {
+        x: (rect.left + (rect.width / 2)) - e.clientX,
+        y: (rect.top + (rect.height / 2)) - e.clientY
+      };
+
+      var rotation = Math.atan2(mouse.x, mouse.y) * 180 / Math.PI;
+
+      this.blueLeftEye.nativeElement.style.setProperty('--deg', (-rotation + eyeOffset) + 'deg');
+    }
+
+    if (this.blueRightEye) {
+      var rect = this.blueRightEye.nativeElement.getBoundingClientRect();
+
+      var mouse = {
+        x: (rect.left + (rect.width / 2)) - e.clientX,
+        y: (rect.top + (rect.height / 2)) - e.clientY
+      };
+
+      var rotation = Math.atan2(mouse.x, mouse.y) * 180 / Math.PI;
+
+      this.blueRightEye.nativeElement.style.setProperty('--deg', (-rotation + eyeOffset) + 'deg');
+    }
+  }
+}
