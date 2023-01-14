d0a7705179b617968e3b9ce18e04c7fd05fc7225
Matthew Berry <mjberry@illinois.edu>
2023-01-14 10:42:22 -0600

    added comment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,12 +12,14 @@ export class AppComponent {
 
   title = 'Digital Molecule Maker';
 
-  // TODO consider replacing this with nested routes
+  // TODO consider replacing this with nested routes to control
+  // container elements
   showHeader$: Observable<boolean>;
 
   constructor(
     private router: Router
   ) {
+    // show headers when we're not on the splash page
     this.showHeader$ = this.router.events.pipe(
       filter(event => event instanceof NavigationEnd),
       map(event => !['/', '/activity'].includes((event as NavigationEnd).url))
