43dac88b9cecdb09e38af9c3766c261cb6de13f4
Wenqi He <hewenqi96@gmail.com>
2023-10-30 12:16:55 -0500

    make global toggle reset block local states

--- src/app/block/block.component.ts
+++ src/app/block/block.component.ts
@@ -68,6 +68,7 @@ export class BlockComponent implements OnInit {
   ) {
     workspaceService.functionMode$.subscribe((enabled) => {
       this._functionModeEnabled = enabled;
+      this.flipped = false;
       this.cd.markForCheck();
     });
   }
