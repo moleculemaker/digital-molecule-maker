ef0e5987e436733e5df3e4250b5d5143cb217273
Wenqi He <galmungral@gmail.com>
2024-04-22 16:38:44 -0500

    move toggle button & fix reset button

--- src/app/app-build/app-build.component.ts
+++ src/app/app-build/app-build.component.ts
@@ -46,15 +46,12 @@ export class AppBuildComponent implements OnInit {
   moleculeList: Molecule[] = [];
 
   hoveredMolecule?: number = undefined;
-  spacebarPressed = false;
 
   panning = false;
   private _initialPosition!: { x: number; y: number };
-  private _panElement!: HTMLElement;
   closeOverlay: Subject<void> = new Subject<void>();
-  isDragging: boolean | undefined;
-  startingPointerPosition: { x: number; y: number } = { x: 0, y: 0 };
-  draggedMoleculeIndex: number | undefined;
+
+  functionModeEnabled = true;
 
   constructor(
     private rigService: RigService,
@@ -74,6 +71,9 @@ export class AppBuildComponent implements OnInit {
       if (blockSet) {
         this.svgScale = getBlockSetScale(blockSet, 70);
       }
+    });
+    this.workspaceService.functionMode$.subscribe((enabled) => {
+      this.functionModeEnabled = enabled;
     });
   }
 
@@ -97,18 +97,10 @@ export class AppBuildComponent implements OnInit {
         this.moleculeList = moleculeList;
         this.changeDetector.detectChanges();
       });
+  }
 
-    document.addEventListener('keydown', (event) => {
-      if (event.code === 'Space') {
-        this.spacebarPressed = true;
-      }
-    });
-
-    document.addEventListener('keyup', (event) => {
-      if (event.code === 'Space') {
-        this.spacebarPressed = false;
-      }
-    });
+  toggle() {
+    this.workspaceService.toggle();
   }
 
   get personalCart$() {
@@ -162,8 +154,7 @@ export class AppBuildComponent implements OnInit {
   }
 
   onCenter(): void {
-    this.zoomAndPanMatrix[4] = 0;
-    this.zoomAndPanMatrix[5] = 0;
+    this.zoomAndPanMatrix = [1, 0, 0, 1, 0, 0];
   }
 
   /**
@@ -199,15 +190,14 @@ export class AppBuildComponent implements OnInit {
         this.hoveredMolecule
       ]!.blockList.filter((block) => block.index != event.data.index);
       this.moleculeList[this.hoveredMolecule]!.blockList.push(event.data);
-    } else {
-      // if (event.data.type == BlockType.Start) {
+    } else if (this.moleculeList.length === 0) {
+      // disable multi-molecule for now
       const newBlockList: Block[] = [event.data];
       const pos = this.getPointerPosition(event.nativeEvent);
       const { x, y } = this.invertTransforms(pos.x, pos.y);
       const positionCoordinates = new Coordinates(x, y);
       const newMolecule = new Molecule(positionCoordinates, newBlockList);
       this.moleculeList.push(newMolecule);
-      // }
     }
     // todo: clean up state management a bit; currently modifying the object in place, passing the
     // object to the service, and then subscribing to the service for updates
@@ -227,46 +217,22 @@ export class AppBuildComponent implements OnInit {
   onPanStart(event: PointerEvent) {
     this.closeOverlay.next();
 
-    if (this.spacebarPressed) {
-      this.panning = true;
-      this._panElement = event.target as HTMLElement;
-      this.closeOverlay.next();
+    this.panning = true;
+    this.closeOverlay.next();
 
-      this._initialPosition = {
-        x: event.pageX - this.zoomAndPanMatrix[4]!,
-        y: event.pageY - this.zoomAndPanMatrix[5]!,
-      };
-    }
+    this._initialPosition = {
+      x: event.pageX - this.zoomAndPanMatrix[4]!,
+      y: event.pageY - this.zoomAndPanMatrix[5]!,
+    };
   }
 
   onPan(event: PointerEvent) {
-    if (this.panning && this.spacebarPressed) {
+    if (this.panning) {
       let dx = event.pageX - this._initialPosition.x;
       let dy = event.pageY - this._initialPosition.y;
 
       this.zoomAndPanMatrix[4] = dx;
       this.zoomAndPanMatrix[5] = dy;
-    } else {
-      // Original code for single molecule movement
-      const moleculeIndex = this.hoveredMolecule;
-      if (
-        this.isDragging &&
-        typeof moleculeIndex !== 'undefined' &&
-        !this.spacebarPressed
-      ) {
-        const pointerPosition = this.getPointerPosition(event);
-        const dx =
-          (pointerPosition.x - this.startingPointerPosition.x) /
-          this.zoomAndPanMatrix[0]!;
-        const dy =
-          (pointerPosition.y - this.startingPointerPosition.y) /
-          this.zoomAndPanMatrix[3]!;
-
-        this.moleculeList[moleculeIndex]!.position.x += dx;
-        this.moleculeList[moleculeIndex]!.position.y += dy;
-
-        this.startingPointerPosition = pointerPosition;
-      }
     }
   }
 
@@ -282,46 +248,6 @@ export class AppBuildComponent implements OnInit {
   onRemoveBlock(moleculeId: number, blockIndex: number) {
     this.hoveredMolecule = undefined;
     this.workspaceService.removeBlock(moleculeId, blockIndex);
-  }
-
-  closeMoleculePopup() {
-    this.closeOverlay.next();
-  }
-
-  onMoveStart(event: PointerEvent, moleculeIndex: number) {
-    this.isDragging = true;
-    this.draggedMoleculeIndex = moleculeIndex;
-    this.startingPointerPosition = this.getPointerPosition(event);
-    this.closeMoleculePopup();
-  }
-
-  onMove(event: PointerEvent) {
-    if (
-      this.isDragging &&
-      typeof this.draggedMoleculeIndex !== 'undefined' &&
-      !this.spacebarPressed
-    ) {
-      const pointerPosition = this.getPointerPosition(event);
-      const dx =
-        (pointerPosition.x - this.startingPointerPosition.x) /
-        this.zoomAndPanMatrix[0]!;
-      const dy =
-        (pointerPosition.y - this.startingPointerPosition.y) /
-        this.zoomAndPanMatrix[3]!;
-
-      this.moleculeList[this.draggedMoleculeIndex]!.position.x += dx;
-      this.moleculeList[this.draggedMoleculeIndex]!.position.y += dy;
-
-      this.startingPointerPosition = pointerPosition;
-    }
-  }
-
-  @HostListener('pointerup', ['$event'])
-  onMoveStop(event: PointerEvent) {
-    if (this.isDragging) {
-      this.isDragging = false;
-      this.draggedMoleculeIndex = undefined;
-    }
   }
 
   getPointerPosition(event: PointerEvent) {
