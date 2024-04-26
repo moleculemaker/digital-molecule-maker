95ddb8e82b78e35fdd54149053b52105330e9be9
Wenqi He <galmungral@gmail.com>
2024-04-26 15:51:14 -0500

    fix detail panel stale state

--- src/app/detail-panel/detail-panel.component.ts
+++ src/app/detail-panel/detail-panel.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewChecked, Component, ElementRef } from '@angular/core';
+import {AfterViewChecked, Component, ElementRef, TrackByFunction} from '@angular/core';
 import { WorkspaceService } from '../services/workspace.service';
 import { CartService } from '../services/cart.service';
 import { lookupProperty } from '../lookup';
@@ -13,7 +13,6 @@ import { UserService } from '../services/user.service';
 export class DetailPanelComponent implements AfterViewChecked {
   block: Block | null = null;
   molecule: Molecule | null = null;
-  moleculesWithSingleBlocks: Molecule[] = [];
 
   shouldScroll = false;
 
@@ -26,19 +25,27 @@ export class DetailPanelComponent implements AfterViewChecked {
     this.workspaceService.selectedMolecule$.subscribe((molecule) => {
       if (molecule) {
         this.molecule = molecule;
-        this.moleculesWithSingleBlocks = [];
-        molecule.blockList.sort((a, b) => a.index - b.index);
-        for (let block of molecule.blockList) {
-          this.moleculesWithSingleBlocks.push(
-            new Molecule(new Coordinates(0, 0), [block], ''),
-          );
-        }
       }
     });
     this.workspaceService.selectedBlock$.subscribe((block) => {
       this.block = block;
       this.shouldScroll = true;
     });
+  }
+
+  trackByBlockIndexId: TrackByFunction<Molecule> = (index, molecule) => {
+    const block = molecule.blockList[0];
+    return `${block?.index}:${block?.id}`
+  }
+
+  get moleculesWithSingleBlocks(): Molecule[] {
+    if (!this.molecule) return [];
+    const res: Molecule[] = [];
+    this.molecule.blockList.sort((a, b) => a.index - b.index);
+    for (let block of this.molecule.blockList) {
+      res.push(new Molecule(new Coordinates(0, 0), [block], ''));
+    }
+    return res;
   }
 
   get blockSet$() {
