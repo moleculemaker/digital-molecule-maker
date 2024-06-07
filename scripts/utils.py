dc2261c1857783a57ecba1c34f4634da4e81502c
Wenqi He <galmungral@gmail.com>
2024-06-07 15:00:22 -0500

    reprocess block set data

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,16 +1,7 @@
-import functools
-import operator
 import os
-import re
-from collections import Counter
 from math import ceil
 from xml.dom.minidom import parse
-
-
-def combine(key, initial):
-    def combine_property(*blocks):
-        return functools.reduce(operator.add, [block['properties'][key] for block in blocks if block], initial)
-    return combine_property
+from rdkit.Chem import CombineMols, MolFromSmiles
 
 
 def get_svg_dimensions(url):
@@ -19,49 +10,8 @@ def get_svg_dimensions(url):
     return [ceil(float(v)) for v in svg_el.getAttribute('viewBox').split(' ')]
 
 
-def combine_chemical_formulas(*blocks):
-    concatenated_formula = ''.join(block['properties']['chemicalFormula'] for block in blocks if block)
-
-    if not concatenated_formula:
-        return ''
-
-    atom_counts = Counter()
-
-    def process_substring(substring):
-        match = re.search(r'([A-Za-z]+)(\d*)', substring)
-        if match:
-            atom = match.group(1)
-            count = int(match.group(2) or '1')
-            atom_counts[atom] += count
-
-    seen = concatenated_formula[0]
-
-    for char in concatenated_formula[1:]:
-        # if it's an upper-case letter, process seen and start anew
-        if char.isupper():
-            process_substring(seen)
-            seen = char
-        else:
-            seen += char
-
-    process_substring(seen)
-
-    ret_val = ''
-
-    def append_one_atom(atom):
-        nonlocal ret_val
-        if atom in atom_counts:
-            count = atom_counts[atom]
-            ret_val += atom
-            if count > 1:
-                ret_val += str(count)
-
-    for atom in ['C', 'H', 'F', 'N', 'O', 'S']:
-        append_one_atom(atom)
-        del atom_counts[atom]
-
-    # if there's anything left, add it to the end
-    for atom in atom_counts:
-        append_one_atom(atom)
-
-    return ret_val
+def naive_combine(all_smiles):
+    mol = MolFromSmiles('')
+    for smiles in all_smiles:
+        mol = CombineMols(mol, MolFromSmiles(smiles))
+    return mol
