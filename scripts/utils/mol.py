7c8dd3fa6cd32e501fd256eee9bd5c17e0db0c86
sarthakc123 <src16@illinois.edu>
2025-10-24 13:26:45 -0500

    Commit Samys12 POC

--- scripts/utils/mol.py
+++ scripts/utils/mol.py
@@ -6,8 +6,36 @@ from rdkit.Chem import CombineMols, MolFromSmiles
 from openbabel import openbabel
 from tqdm import tqdm
 
-from config import config
+#from config import config
+from enum import Enum
+from os import path
 
+
+class BlockSet(Enum):
+    ColorWheel = "ColorWheel_20230504"
+    OPV = "OPV_20230504"
+    Chem437 = "Chem_437"
+    Samys12 = "Samys12"
+
+
+class Config:
+    block_set = BlockSet.Samys12
+    src_dir = path.join(path.dirname(__file__), "../src")
+
+    @property
+    def block_set_id(self):
+        return self.block_set.value
+
+    @property
+    def workdir(self):
+        return path.abspath(
+            path.join(
+                path.dirname(__file__), "../src/assets/blocks", self.block_set.value
+            )
+        )
+
+
+config = Config()
 
 def naive_combine(all_smiles):
     mol = MolFromSmiles("")
